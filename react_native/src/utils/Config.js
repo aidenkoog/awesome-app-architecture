20f6df789072dbaf93e4a08708b1611fb8e4ea27
aidenkoog <k037686@naver.com>
2022-10-24 12:06:50 +0900

    [Feature] Delete service uuid used to scan device

--- react_native/src/utils/Config.js
+++ react_native/src/utils/Config.js
@@ -1,4 +1,4 @@
-export const SERVICE_UUID = '0783b03e-8535-b5a0-7140-a304d2495cb7';
+export const SERVICE_UUID = '';
 export const RX_CHARACTERISTIC_UUID = '';
 export const TX_CHARACTERISTIC_UUID = '';
 export const FLOW_CONTROL_CHARACTERISTIC_UUID = '';
