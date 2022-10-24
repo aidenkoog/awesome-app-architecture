bc9fc10dccf3d70384a7949c84e3ed5716961b77
aidenkoog <k037686@naver.com>
2022-10-24 12:09:24 +0900

    [Feature] Change log tag text style (--> uppercase)

--- react_native/src/utils/Constants.js
+++ react_native/src/utils/Constants.js
@@ -15,32 +15,32 @@ export default {
     },
     LOG: {
         // ble manager log
-        BT_BLE_MANAGER: 'bt_ble_manager',
+        BT_BLE_MANAGER: 'BT_BLE_MANAGER',
 
         // repository log
-        BT_REPO_LOG: 'bt_repo_log',
-        COMMON_REPO_LOG: 'common_repo_log',
-        PLATFORM_REPO_LOG: 'platform_repo_log',
-        SERVER_REPO_LOG: 'server_repo_log',
+        BT_REPO_LOG: 'BT_REPO_LOG',
+        COMMON_REPO_LOG: 'COMMON_REPO_LOG',
+        PLATFORM_REPO_LOG: 'PLATFORM_REPO_LOG',
+        SERVER_REPO_LOG: 'SERVER_REPO_LOG',
 
         // usecase log
-        BT_USECASE_LOG: 'bt_usecase_log',
-        COMMON_USECASE_LOG: 'common_usecase_log',
-        PLATFORM_USECASE_LOG: 'platform_usecase_log',
-        SERVER_USECASE_LOG: 'server_usecase_log',
+        BT_USECASE_LOG: 'BT_USECASE_LOG',
+        COMMON_USECASE_LOG: 'COMMON_USECASE_LOG',
+        PLATFORM_USECASE_LOG: 'PLATFORM_USECASE_LOG',
+        SERVER_USECASE_LOG: 'SERVER_USECASE_LOG',
 
         // ui log
-        BT_UI_LOG: 'bt_ui_log',
-        HOME_UI_LOG: 'home_ui_log',
-        PROFILE_UI_LOG: 'profile_ui_log',
-        ROOT_UI_LOG: 'root_ui_log',
-        SETTINGS_UI_LOG: 'settings_ui_log',
-        SPLASH_UI_LOG: 'splash_ui_log',
+        BT_UI_LOG: 'BT_UI_LOG',
+        HOME_UI_LOG: 'HOME_UI_LOG',
+        PROFILE_UI_LOG: 'PROFILE_UI_LOG',
+        ROOT_UI_LOG: 'ROOT_UI_LOG',
+        SETTINGS_UI_LOG: 'SETTINGS_UI_LOG',
+        SPLASH_UI_LOG: 'SPLASH_UI_LOG',
 
         // test log
-        REPO_TEST_LOG: 'repo_test_log',
-        UI_TEST_LOG: 'ui_test_log',
-        SOURCE_TEST_LOG: 'source_test_log',
-        USECASE_TEST_LOG: 'usecase_test_log'
+        REPO_TEST_LOG: 'REPO_TEST_LOG',
+        UI_TEST_LOG: 'UI_TEST_LOG',
+        SOURCE_TEST_LOG: 'SOURCE_TEST_LOG',
+        USECASE_TEST_LOG: 'USECASE_TEST_LOG'
     }
 };
