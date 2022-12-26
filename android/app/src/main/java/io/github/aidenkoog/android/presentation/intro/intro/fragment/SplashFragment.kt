1287b2f76ef73afab59a0ad31840f6f3d3a19b87
AidenKoo <k037686@naver.com>
2022-12-26 14:42:48 +0900

    [Feature] Add splash loading lottie animation

--- android/app/src/main/java/io/github/aidenkoog/android/presentation/intro/intro/fragment/SplashFragment.kt
+++ android/app/src/main/java/io/github/aidenkoog/android/presentation/intro/intro/fragment/SplashFragment.kt
@@ -1,5 +1,6 @@
 package io.github.aidenkoog.android.presentation.intro.intro.fragment
 
+import android.animation.Animator
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
@@ -10,9 +11,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.orhanobut.logger.Logger
 import io.github.aidenkoog.android.BR
+import io.github.aidenkoog.android.R
 import io.github.aidenkoog.android.databinding.FragmentSplashBinding
 import io.github.aidenkoog.android.presentation.base.fragment.BaseFragment
 import io.github.aidenkoog.android.presentation.intro.intro.viewmodel.SplashViewModel
+import io.github.aidenkoog.android.util.LottieUtil
 
 class SplashFragment : BaseFragment() {
 
@@ -31,12 +34,39 @@ class SplashFragment : BaseFragment() {
         viewDataBinding = FragmentSplashBinding.inflate(inflater, container, false)
         viewDataBinding?.setVariable(BR.splashViewModel, viewModelData)
         viewDataBinding?.executePendingBindings()
+        setLottieAnimation()
         return viewDataBinding?.root!!
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Logger.d("onViewCreated:")
+    }
+
+    private fun setLottieAnimation() {
+        val loadingImageView = viewDataBinding?.loadingSplashImageview!!
+            LottieUtil.setLottieRawResource(
+                loadingImageView,
+                R.raw.splash_lottie
+            )
+        LottieUtil.addLottieAnimatorListener(loadingImageView, lottieAnimationListener)
+        loadingImageView.let { LottieUtil.setLottieSpeed(it, 1.0f) }
+        loadingImageView.let { LottieUtil.playLottie(it, 3000) }
+    }
+
+    private val lottieAnimationListener = object: LottieUtil.LottieAnimatorListener {
+        override fun onAnimationStart(animator: Animator?) {
+        }
+
+        override fun onAnimationEnd(animator: Animator?) {
+        }
+
+        override fun onAnimationCancel(animator: Animator?) {
+        }
+
+        override fun onAnimationRepeat(animator: Animator?) {
+        }
+
     }
 
     fun movePlayStore() {
