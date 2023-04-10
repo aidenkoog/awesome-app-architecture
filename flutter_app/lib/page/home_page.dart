1fcca9e517732ac133b8cb68d20ad74cf61be7c8
AidenKooG <k037686@gmail.com>
2023-04-10 23:24:23 +0900

    [Feature] Delete all files related to flutter app

--- flutter_app/lib/page/home_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class HomePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-}
