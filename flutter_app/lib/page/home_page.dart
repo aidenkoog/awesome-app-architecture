29c9ab47f7542d3708f7c3523c0514f4b8737f86
AidenKoo <k037686@naver.com>
2023-04-10 19:06:35 +0900

    [Feature] Add toast test code to flutter app project

---
+++ flutter_app/lib/page/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
