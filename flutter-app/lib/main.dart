341a12641b20d0bd636fa9fee258b578d65dfb03
AidenKoo <k037686@naver.com>
2023-05-10 17:54:48 +0900

    feat: add template function to add getx controller and code which initializes GetStorage

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'presentation/pages/splash/splash_page.dart';
 
-void main() => runApp(const App());
+void main() async {
+  await GetStorage.init();
+  runApp(const App());
+}
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -13,6 +17,12 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  static void putControllers() {
+    // Get.put();
+    // Get.lazyPut();
+    // Get.lazyPut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
