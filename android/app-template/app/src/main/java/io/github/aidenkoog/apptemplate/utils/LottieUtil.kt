441a2689b668315e3aba066fd3bb0a6fe2e70309
AidenKooG <k037686@gmail.com>
2023-11-27 17:11:22 +0900

    feat: add lottie dependency & utility code / add code which can monitor network status

---
+++ android/app-template/app/src/main/java/io/github/aidenkoog/apptemplate/utils/LottieUtil.kt
@@ -0,0 +1,107 @@
+package io.github.aidenkoog.apptemplate.utils
+
+import android.animation.Animator
+import android.view.View
+import android.widget.ImageView
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.RenderMode
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+object LottieUtil {
+
+    fun setLottieRawResource(view: LottieAnimationView, rawRes: Int) {
+        view.setAnimation(rawRes)
+    }
+
+    fun setLottieRenderMode(view: LottieAnimationView, renderMode: RenderMode) {
+        view.renderMode = renderMode
+    }
+
+    fun playLottie(view: LottieAnimationView) {
+        view.visibility = View.VISIBLE
+        view.playAnimation()
+    }
+
+    fun playLottie(view: LottieAnimationView, duration: Long) {
+        view.playAnimation()
+        CoroutineScope(Dispatchers.IO).launch {
+            withContext(Dispatchers.IO) {
+                Thread.sleep(duration)
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                view.cancelAnimation()
+                view.visibility = View.GONE
+            }
+        }
+    }
+
+    fun pauseLottie(view: LottieAnimationView) {
+        view.pauseAnimation()
+    }
+
+    fun cancelLottie(view: LottieAnimationView) {
+        Timber.d("cancelLottile!")
+        view.cancelAnimation()
+        view.visibility = View.GONE
+    }
+
+    fun cancelLottieWithInvisible(view: LottieAnimationView) {
+        view.cancelAnimation()
+        view.visibility = View.INVISIBLE
+    }
+
+    fun setLottieRepeatCount(view: LottieAnimationView, repeatCount: Int) {
+        view.repeatCount = repeatCount
+    }
+
+    fun setLottieSpeed(view: LottieAnimationView, speed: Float) {
+        view.speed = speed
+    }
+
+    fun getLottieSpeed(view: LottieAnimationView): Float {
+        return view.speed
+    }
+
+    fun setScaleType(view: LottieAnimationView, scaleType: ImageView.ScaleType) {
+        view.scaleType = scaleType
+    }
+
+    fun isLottieAnimating(view: LottieAnimationView): Boolean {
+        return view.isAnimating
+    }
+
+    interface LottieAnimatorListener {
+        fun onAnimationStart(animator: Animator?)
+        fun onAnimationEnd(animator: Animator?)
+        fun onAnimationCancel(animator: Animator?)
+        fun onAnimationRepeat(animator: Animator?)
+    }
+
+    fun addLottieAnimatorListener(view: LottieAnimationView, listener: LottieAnimatorListener?) {
+        view.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animator: Animator) {
+                Timber.i("Lottie animation is started")
+                listener?.onAnimationStart(animator)
+            }
+
+            override fun onAnimationEnd(animator: Animator) {
+                Timber.i("Lottie animation is ended")
+                listener?.onAnimationEnd(animator)
+            }
+
+            override fun onAnimationCancel(animator: Animator) {
+                Timber.i("Lottie animation is canceled")
+                listener?.onAnimationCancel(animator)
+            }
+
+            override fun onAnimationRepeat(animator: Animator) {
+                Timber.i("Lottie animation is repeated")
+                listener?.onAnimationRepeat(animator)
+            }
+        })
+    }
+}
---
+++ android/app-template/app/src/main/java/io/github/aidenkoog/apptemplate/utils/NetworkStatusHelper.kt
@@ -0,0 +1,65 @@
+package io.github.aidenkoog.apptemplate.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.lifecycle.LiveData
+
+sealed class NetworkStatus {
+    object Available : NetworkStatus()
+    object Unavailable : NetworkStatus()
+}
+
+class NetworkStatusHelper(context: Context) : LiveData<NetworkStatus>() {
+
+    val validNetworkConnections: ArrayList<Network> = ArrayList()
+    var connectivityManager: ConnectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private lateinit var connectivityManagerCallback: ConnectivityManager.NetworkCallback
+
+    fun announceStatus() {
+        if (validNetworkConnections.isNotEmpty()) {
+            postValue(NetworkStatus.Available)
+        } else {
+            postValue(NetworkStatus.Unavailable)
+        }
+    }
+
+    private fun getConnectivityManagerCallback() = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            val networkCapability = connectivityManager.getNetworkCapabilities(network)
+            val hasNetworkConnection =
+                networkCapability?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                    ?: false
+            validNetworkConnections.clear()
+            if (hasNetworkConnection) {
+                validNetworkConnections.add(network)
+                announceStatus()
+            }
+        }
+
+        override fun onLost(network: Network) {
+            validNetworkConnections.clear()
+            announceStatus()
+            super.onLost(network)
+        }
+    }
+
+    override fun onActive() {
+        super.onActive()
+        connectivityManagerCallback = getConnectivityManagerCallback()
+        val networkRequest =
+            NetworkRequest.Builder().addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build()
+        connectivityManager.registerNetworkCallback(networkRequest, connectivityManagerCallback)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        connectivityManager.unregisterNetworkCallback(connectivityManagerCallback)
+    }
+}
