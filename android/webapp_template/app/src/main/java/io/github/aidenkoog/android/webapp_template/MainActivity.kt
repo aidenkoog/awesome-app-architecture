eab6b31d866a8a6ef2fec57f230ab67223275c93
AidenKooG <k037686@gmail.com>
2023-12-12 01:42:12 +0900

    feat: add webview template which uses html asset and load it

--- android/webapp_template/app/src/main/java/io/github/aidenkoog/android/webapp_template/MainActivity.kt
+++ android/webapp_template/app/src/main/java/io/github/aidenkoog/android/webapp_template/MainActivity.kt
@@ -1,11 +1,48 @@
 package io.github.aidenkoog.android.webapp_template
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
 
+
+/*
+ * template web view source code which loads html assets.
+ */
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        webView = WebView(this)
+        webView.clearCache(false)
+
+        val contentUrl = getInternalHtmlResource()
+        contentUrl?.let {
+            webView.loadUrl(it)
+        }
+        setContentView(webView)
     }
+
+    private fun getInternalHtmlResource(): String? {
+        val items: Array<String>?
+        try {
+            items = assets.list("html")
+            items?.let {
+                for (item in it) {
+                    if (item == "index.html") {
+                        return "file:///android_asset/html/index.html"
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("debug", "there's no any asset file !!!")
+            e.printStackTrace()
+        }
+        Log.d("debug", "cannot find index.html !!!")
+        return null
+    }
+
 }
