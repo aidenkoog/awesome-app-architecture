455954b358ba1da309089e90cc4f6eddf7312438
AidenKooG <k037686@gmail.com>
2023-12-13 19:33:55 +0900

    bugfix: fix app's build error and add dependencies for testing, HiltAndroidRule etc

--- android/app-template/app/src/androidTest/java/io/github/aidenkoog/apptemplate/usecase/intro/IntroUseCaseTest.kt
+++ android/app-template/app/src/androidTest/java/io/github/aidenkoog/apptemplate/usecase/intro/IntroUseCaseTest.kt
@@ -14,7 +14,6 @@ import io.github.aidenkoog.apptemplate.domain.usecase.feature.UserUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
