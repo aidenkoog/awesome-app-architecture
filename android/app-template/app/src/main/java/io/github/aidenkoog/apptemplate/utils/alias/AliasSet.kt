a783c590fbf562c2bda3f1ed204572beec030363
AidenKooG <k037686@gmail.com>
2023-12-14 01:13:41 +0900

    feat: add typealias test code

---
+++ android/app-template/app/src/main/java/io/github/aidenkoog/apptemplate/utils/alias/AliasSet.kt
@@ -0,0 +1,17 @@
+package io.github.aidenkoog.apptemplate.utils.alias
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+/*
+ * typealias example code.
+ */
+@SinceKotlin("1.1")
+typealias BooleanLiveData = LiveData<Boolean>
+
+
+@SinceKotlin("1.1")
+typealias BooleanMutableLiveData = MutableLiveData<Boolean>
+
+// test code
+typealias AnyLiveData = LiveData<Any>
